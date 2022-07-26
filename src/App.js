c8b8a8e5e31eded321211882df5cd879cbe525f7
JohnKamaujk <johnjkkamau@gmail.com>
2022-07-26 08:23:59 +0300

    Added an process env for storing my API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { TbMapSearch } from "react-icons/tb";
+import { TbSearch } from "react-icons/tb";
+import { useState } from "react";
+import Header from "./components/Header";
+import DetailsCard from "./components/DetailsCard";
+import SummaryCard from "./components/SummaryCard";
 
 function App() {
   return <div className="App"></div>;
