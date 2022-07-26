6e9005deef924024b40acf550c0266a717fd9699
JohnKamaujk <johnjkkamau@gmail.com>
2022-07-26 09:18:23 +0300

    Added states that will be used

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ import DetailsCard from "./components/DetailsCard";
 import SummaryCard from "./components/SummaryCard";
 
 function App() {
-  return <div className="App"></div>;
+    const API_KEY = process.env.REACT_APP_API_KEY;
+
+    const [noData, setNoData] = useState("No Data Yet");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [weatherData, setWeatherData] = useState([]);
+    const [city, setCity] = useState("Unknown location");
+    const [weatherIcon, setWeatherIcon] = useState(
+      `${process.env.REACT_APP_ICON_URL}10n@2x.png`
+    );
+
+    const handleChange = (input) => {
+      const { value } = input.target;
+      setSearchTerm(value);
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      getWeather(searchTerm);
+    };
 }
 
 export default App;
